5f2e296b40c042af8f77ddffe1889ce667938204
Isklar <mrisklar@gmail.com>
2016-02-02 02:45:01 +0000

    Complete rewrite to use the simpler bot interface from discordpy

--- dbot.py
+++ dbot.py
@@ -4,55 +4,62 @@ import random
 import options
 import string
 
-username = options.username
-password = options.password
 
-client = discord.Client()
-client.login(username, password)
+description = '''D&D Dice rolling bot.'''
+bot = commands.Bot(command_prefix='.', description=description)
 
-
-# Connection confirmation
-@client.event
+@bot.event
 async def on_ready():
-    print('Connected!')
-    print('Username: ' + client.user.name)
-    print('ID: ' + client.user.id)
-    print("------------------")
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    
+@bot.event
+async def on_command_error(error, ctx):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await bot.send_message(ctx.message.author, 'Incorrect usage. Usage: .r XdX | .r 5d20')
     
 
 async def delete_messages(message, author):
-    async for historicMessage in client.logs_from(message.channel):
-        if historicMessage.author == client.user:
+    async for historicMessage in bot.logs_from(message.channel):
+        if historicMessage.author == bot.user:
             if (author.name in historicMessage.content) or (author.mention in historicMessage.content):
-                await client.delete_message(historicMessage)
+                await bot.delete_message(historicMessage)
             
         if historicMessage.content.startswith('.r'):
             if author == historicMessage.author:
                 try:
-                   await client.delete_message(historicMessage)
+                   await bot.delete_message(historicMessage)
                 except:
-                   print('Error: Cannot delete messages!')  
-  
-async def roll(message, single=True):
+                   print('Error: Cannot delete message!')  
 
+
+@bot.command(pass_context=True)
+async def r(ctx, dice : str):
+    """Rolls a dice using XdX format.
+    e.g .r 3d6"""
+    
     resultTotal = 0
     resultString = ''
     
-    if single:
-        dice = '1d20'
-    else:
-        dice = message.content[3:] #strip .r
-        
-    numDice = dice.split('d')[0]
-    diceVal = dice.split('d')[1]
-    await client.send_message(message.channel, "Rolling %s d%s for %s" % (numDice, diceVal, message.author))
-    
-    try:
-        rolls, limit = map(int, dice.split('d'))
+    try: 
+        numDice = dice.split('d')[0]
+        diceVal = dice.split('d')[1]
     except Exception:
-        await client.send_message(message.channel, 'Format has to be in xdx!')
+        await bot.say("Format has to be in xdx %s." % ctx.message.author)
         return
-        
+
+    if int(numDice) > 500:
+        await bot.say("I cant roll that many dice %s." % ctx.message.author)
+        return
+
+    await delete_messages(ctx.message, ctx.message.author)
+    
+    bot.type()
+    await bot.say("Rolling %s d%s for %s" % (numDice, diceVal, ctx.message.author))
+    rolls, limit = map(int, dice.split('d'))
+
     for r in range(rolls):
         number = random.randint(1, limit)
         resultTotal = resultTotal + number
@@ -61,25 +68,11 @@ async def roll(message, single=True):
             resultString += str(number)
         else:
             resultString += ', ' + str(number)
-
-    await client.send_message(message.channel, message.author.mention + "  :game_die:\n**Result:** " + resultString + "\n**Total:** " + str(resultTotal) + "\n─────────────────────────")
     
-@client.event
-async def on_message(message):
-    if message.author.id != client.user.id:
-        if message.content.startswith('.r'):
-            command = message.content[3:]
-            
-            if not command:
-                await delete_messages(message, message.author)
-                await roll(message)
-            
-            elif command != 'help':
-                await delete_messages(message, message.author)
-                await roll(message, False)
-            
-            else:
-                await client.send_message(message.channel, "Usage: .r xdx")
-                await client.send_message(message.channel, "e.g. `.r 3d20`")
-        
-client.run(username, password)
+    if numDice == '1':
+        await bot.say(ctx.message.author.mention + "  :game_die:\n**Result:** " + resultString + "\n─────────────────────────")
+    else:
+        await bot.say(ctx.message.author.mention + "  :game_die:\n**Result:** " + resultString + "\n**Total:** " + str(resultTotal) + "\n─────────────────────────")
+
+
+bot.run(options.username, options.password)
