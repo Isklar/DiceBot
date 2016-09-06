f437997157b2e31fc6e7917801309f73f0fecf04
Isklar <mrisklar@gmail.com>
2016-09-06 23:59:04 +0100

    Fix roll command and add some exception output.

--- dbot.py
+++ dbot.py
@@ -43,38 +43,42 @@ async def r(ctx, roll : str):
     
     resultTotal = 0
     resultString = ''
-    
-    try: 
-        numDice = roll.split('d')[0]
-        diceVal = roll.split('d')[1]
+    try:
+        try: 
+            numDice = roll.split('d')[0]
+            diceVal = roll.split('d')[1]
+        except Exception as e:
+            print(e)
+            await bot.say("Format has to be in #d# %s." % ctx.message.author.name)
+            return
+
+        if int(numDice) > 500:
+            await bot.say("I cant roll that many dice %s." % ctx.message.author.name)
+            return
+
+        await delete_messages(ctx.message, ctx.message.author)
+        
+        bot.type()
+        await bot.say("Rolling %s d%s for %s" % (numDice, diceVal, ctx.message.author.name))
+        rolls, limit = map(int, roll.split('d'))
+
+        for r in range(rolls):
+            number = random.randint(1, limit)
+            resultTotal = resultTotal + number
+            
+            if resultString == '':
+                resultString += str(number)
+            else:
+                resultString += ', ' + str(number)
+        
+        if numDice == '1':
+            await bot.say(ctx.message.author.mention + "  :game_die:\n**Result:** " + resultString)
+        else:
+            await bot.say(ctx.message.author.mention + "  :game_die:\n**Result:** " + resultString + "\n**Total:** " + str(resultTotal))
+
     except Exception as e:
         print(e)
-        await bot.say("Format has to be in #d# %s." % ctx.message.author.name)
         return
-
-    if int(numDice) > 500:
-        await bot.say("I cant roll that many dice %s." % ctx.message.author.name)
-        return
-
-    await delete_messages(ctx.message, ctx.message.author)
-    
-    bot.type()
-    await bot.say("Rolling %s d%s for %s" % (numDice, diceVal, ctx.message.author.name))
-    rolls, limit = map(int, dice.split('d'))
-
-    for r in range(rolls):
-        number = random.randint(1, limit)
-        resultTotal = resultTotal + number
-        
-        if resultString == '':
-            resultString += str(number)
-        else:
-            resultString += ', ' + str(number)
-    
-    if numDice == '1':
-        await bot.say(ctx.message.author.mention + "  :game_die:\n**Result:** " + resultString)
-    else:
-        await bot.say(ctx.message.author.mention + "  :game_die:\n**Result:** " + resultString + "\n**Total:** " + str(resultTotal))
 
 @bot.command(pass_context=True)
 async def rt(ctx, roll : str):
